3b9e40e11eee91f799bb566c53922e3f3a19177b
hou <41228208+cowFarmer@users.noreply.github.com>
2022-11-28 21:53:21 +0900

    2022.11.28 update chapter 2-08 bootstrap

--- jump_to_django/pybo/views.py
+++ jump_to_django/pybo/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
 from .models import Question
 
 def index(request):
@@ -10,3 +11,12 @@ def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     context = {'question': question}
     return render(request, 'pybo/question_detail.html', context)
+
+def answer_create(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    # case1
+    question.answer_set.create(content=request.POST.get('content'), create_date=timezone.now())
+    # case2 from .models에서 import Answer를 추가한 후 적용
+    # answer = Answer(question=question, content=request.POST.get('content'), create_date=timezone.now())
+    # answer.save()
+    return redirect('pybo:detail', question_id=question.id)
